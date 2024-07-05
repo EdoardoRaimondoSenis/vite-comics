95001cb0ccc8731416ac1ecad7023ae520b61ac4
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-07-05 17:42:37 +0200

    Aggiunti file di modifica per ogni sezione

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+    import AppFooter from './AppFooter.vue';
+    import AppHeader from './AppHeader.vue';
+    import AppMain from './AppMain.vue';
+
+    export default {
+        name: 'App',
+        components: {
+            AppMain,
+            AppFooter,
+            AppHeader
+        }
+    }
 </script>
 
 <template>
-  
+    
 </template>
 
 <style lang="scss" scoped>
-
+@use '/src/styles/partials/_mixins.scss' as *;
+@use '../src/styles/partials/_variables.scss' as *;
+@use '../src/styles/general.scss';
 </style>
---
+++ src/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: 'AppFooter',
+    }
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '/src/styles/partials/_mixins.scss' as *;
+@use '../src/styles/partials/_variables.scss' as *;
+@use '../src/styles/general.scss';
+</style>
